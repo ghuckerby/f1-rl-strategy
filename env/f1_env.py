de72c857c89559a88fb167d86fb7dde5bde1b8d6
ghuckerby <ghuckerby06@gmail.com>
2025-10-26 10:57:18 +0000

    Initial Commit: Project Setup & Basic RL

---
+++ env/f1_env.py
@@ -0,0 +1,44 @@
+
+import gymnasium as gym
+from gymnasium import spaces
+import numpy as np
+
+class F1PitStopEnv(gym.Env):
+
+    def __init__(self):
+        super().__init__()
+
+        # Action Space
+        # Discrete actions: stay out, pit soft, pit med, pit hard
+        self.action_space = spaces.Discrete(4)
+
+        # Observation Space
+        # Vector with 4 values, each normalised to [0,1]:
+        # lap fraction, tire wear, pit loss, sc active
+        self.observation_space = spaces.Box(
+            low=0,              # min value for each element
+            high=1,             # max value for each element                    
+            shape=(4,),         # 4 values in obs vector
+            dtype=np.float32    # use 32-bit floats
+        )
+        
+        self.state = np.zeros(4)    # vector for obs space
+        self.current_lap = 0        # track current lap
+        self.max_laps = 50          # set race length
+
+    # Called at the start of each new episode (race)
+    def reset(self, *, seed = None, options = None):
+
+        super().reset(seed=seed, options=options)
+        self.current_lap = 0
+        self.state = np.zeros(4)
+        return self.state, {}
+    
+    # Advances environment by one time step, given an action from agent
+    def step(self, action):
+
+        self.current_lap += 1
+        done = self.current_lap >= self.max_laps
+        reward = np.random.randn() # Placeholder random reward
+        self.state = np.random.rand(4) # Placeholder random state
+        return self.state, reward, done, False, {}
