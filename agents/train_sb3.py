0b2516c61bc91cf044d0572d892c3afb5a013969
ghuckerby <ghuckerby06@gmail.com>
2025-10-27 10:47:24 +0000

    Race Logging

--- agents/train_sb3.py
+++ agents/train_sb3.py
@@ -1,11 +1,35 @@
 
 from stable_baselines3 import PPO
 from env.f1_env import F1PitStopEnv
+import pandas as pd
+import numpy as np
 
 def main():
     env = F1PitStopEnv()
     model = PPO("MlpPolicy", env, verbose=1)
     model.learn(total_timesteps=10_000)
+
+    obs, info = env.reset()
+    done = False
+
+    print("\nRace Log\n")
+    while not done:
+        action, _ = model.predict(obs)
+
+        if isinstance(action, np.ndarray):
+            action = int(action.squeeze())
+        else:
+            action = int(action)
+
+        obs, reward, done, truncated, info = env.step(action)
+        env.loggeroutput()
+
+    print("\nRace Summary\n")
+    for lap in env.race_log:
+        print(lap)
+
+    pd.DataFrame(env.race_log).to_csv("race_log.csv", index=False)
+
     model.save("ppo_f1pitstop")
 
 if __name__ == "__main__":
